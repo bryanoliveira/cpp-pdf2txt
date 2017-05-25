2f6a0dbd93fa6dff7311c615b4c3745ee63efe96
mano0012 <luizgustavomattoswow@gmail.com>
2017-05-25 18:36:12 -0300

    visao_2.0

--- visao.cpp
+++ visao.cpp
@@ -94,6 +94,9 @@ void acharJL(Mat p, int linhaI, int linhaF, int *colunaI, int *colunaF){
 			}
 		}
 	}
+	if (*colunaF == p.cols){
+		*colunaF-=1;
+	}
 }
 
 //Função para ajustar a ultima linha com a linha do maior caractere (Exemplo: g)
@@ -106,7 +109,7 @@ void ajusta(Mat p, int *linhaF, int colunaI, int colunaF){
 	while (achou){
 		achou = 0;
 		cond = 1;
-		for (i=colunaI;i<=colunaF && cond == 1;i++){
+		for (i=colunaI;i<=colunaF && cond == 1 && *linhaF+1 < p.rows;i++){
 			vix = p.at<uchar>(*linhaF+1,i);			
 			mm = (int)vix;
 			if (mm == 0){
@@ -114,15 +117,18 @@ void ajusta(Mat p, int *linhaF, int colunaI, int colunaF){
 				achou = 1;
 				cond = 0;
 			}
-			
 		}
+	}
+
+	if (*linhaF == p.rows){
+		*linhaF -= 1;
 	}
 }
 
 
 //Função que encontra os caracteres
 //Retorno: Matriz com somente 1 caractere
-Mat janelaCaractere(Mat p, int linhaI, int linhaF, int *colunaiC, int *colunafC){
+Mat janelaCaractere(Mat p, int linhaI, int linhaF, int *colunaiC, int *colunafC, int colunaF){
 	int i,op,cond,mm;
 	unsigned char vix;
 	
@@ -131,7 +137,7 @@ Mat janelaCaractere(Mat p, int linhaI, int linhaF, int *colunaiC, int *colunafC)
 	while (op){
 		op = 0;
 		cond = 0;
-		for (i=linhaI;i<=linhaF && cond == 0;i++){
+		for (i=linhaI;i<=linhaF && cond == 0 && *colunaiC < colunaF;i++){
 			vix = p.at<uchar>(i,*colunaiC);			
 			mm = (int)vix;
 			if (mm==0){
@@ -139,7 +145,7 @@ Mat janelaCaractere(Mat p, int linhaI, int linhaF, int *colunaiC, int *colunafC)
 			}
 		
 		}
-		if (cond==0){
+		if (cond==0 && *colunaiC<colunaF){
 			*colunaiC = *colunaiC + 1;
 			op = 1;
 		}
@@ -152,7 +158,7 @@ Mat janelaCaractere(Mat p, int linhaI, int linhaF, int *colunaiC, int *colunafC)
 	while (op){
 		op = 0;
 		cond = 0;
-		for (i=linhaI;i<=linhaF && cond == 0;i++){
+		for (i=linhaI;i<=linhaF && cond == 0 && *colunafC+1<=colunaF;i++){
 			vix = p.at<uchar>(i,*colunafC+1);			
 			mm = (int)vix;
 			if (mm == 0){
@@ -210,7 +216,6 @@ void achaCaracteres (Mat p){
 	
 	//Inicializa a primeira linha e a primeira coluna com 0, ou seja, ira começar a procurar a partir do pixel (0,0)
 	primeiral = 0;
-	primeirac = 0;
 	while(primeiral<p.rows){
 		primeirac = 0;
 		ultimac = 0;
@@ -227,39 +232,42 @@ void achaCaracteres (Mat p){
 		//Nesse momento temos a seguinte janela: A[primeiral - primeiral][primeirac - primeirac]
 		//Devemos agora definir até onde o caractere esta escrito, ou seja, a ultima linha e a ultima coluna (conectividade)
 		op = acharJC(p,&primeiral,&ultimal,&primeirac,&ultimac);
-
-		if (op == 0){
+		if (op==0){
 			return;
 		}
-
 		//Agora temos a janela do caractere, que no caso é: A[primeiral - ultimal][primeirac - ultimac]
 		//Vamos então achar a janela da linha
 		acharJL(p,primeiral,ultimal,&primeirac,&ultimac);
 
+	
 
 		/*Neste momento temos a janela da linha, porém, letras como p e q estao cortadas ao meio, pois suas "pernas" ficam
 		abaixo da linha final */
 		//Iremos então ajustar a ultima linha
 		ajusta(p,&ultimal,primeirac,ultimac);
-	
-		primeiracC = primeirac;
-		ultimacC = primeiracC;
-		//Faz o reconhecimento das janelas de cada caractere
-		while (primeiracC<ultimac){
-			//A matriz ver é a matriz de cada caractere
-			ver = janelaCaractere(p, primeiral,ultimal,&primeiracC,&ultimacC);
-			//Redimensiona a janela para ficar 30x20
-			resize(ver, red, Size(30,20), 0, 0, INTER_LINEAR);
-			threshold(red, red, 240, 255,0);
-			//Vetoriza a imagem
-			vetoriza(red,imagem);
-			//Aqui entra o algoritmo do Marcos usando o vetor imagem como parametro
-			
-			//Obs.: As duas proximas linhas SÃO NECESSARIAS
-			ultimacC++;
-			primeiracC = ultimacC;
+		if (primeiral<ultimal && primeirac<ultimac){
+			primeiracC = primeirac;
+			ultimacC = primeiracC;
+			//Faz o reconhecimento das janelas de cada caractere
+			while (primeiracC<ultimac){
+				//A matriz ver é a matriz de cada caractere
+				ver = janelaCaractere(p, primeiral,ultimal,&primeiracC,&ultimacC,ultimac);
+				if (primeiracC<ultimacC && ultimacC){
+					imshow("aa",ver);
+					//Redimensiona a janela para ficar 30x20
+					resize(ver, red, Size(30,20), 0, 0, INTER_LINEAR);
+					imshow("resize",red);
+					waitKey(0);
+					threshold(red, red, 240, 255,0);
+					//Vetoriza a imagem
+					vetoriza(red,imagem);
+					//Aqui entra o algoritmo do Marcos usando o vetor imagem como parametro
+				}
+				//Obs.: As duas proximas linhas SÃO NECESSARIAS
+				ultimacC++;
+				primeiracC = ultimacC;
+			}
 		}
-
 		ultimal++;
 		primeiral = ultimal;
 	}
@@ -275,6 +283,9 @@ int main (int argc, char *argv[]){
 	cvtColor(teste,teste,CV_BGR2GRAY);
 	
 	threshold(teste, teste, 220, 255,0);
+	//thresh = teste(Range(837,903),Range::all());
+	//imshow("tr",thresh);
+	//waitKey(0);
 	achaCaracteres(teste);
 
 	return (0);
